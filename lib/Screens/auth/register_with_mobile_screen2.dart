83eaf1586e22884bf3fec6f0a6d463b8a5f3a3bc
Emad Alhissi <91655568+emadalhissi@users.noreply.github.com>
2022-02-17 22:03:09 -0800

    code send done

--- lib/Screens/auth/register_with_mobile_screen2.dart
+++ lib/Screens/auth/register_with_mobile_screen2.dart
@@ -76,7 +76,7 @@ class _RegisterWithMobileScreen2State extends State<RegisterWithMobileScreen2>
             ElevatedButton(
               onPressed: () async {
                 // await sendOTP();
-                 verifyPhoneNumber();
+                verifyPhoneNumber();
               },
               child: Text('Send OTP'),
               style: ElevatedButton.styleFrom(
@@ -119,13 +119,15 @@ class _RegisterWithMobileScreen2State extends State<RegisterWithMobileScreen2>
         (PhoneAuthCredential phoneAuthCredential) async {
       await auth.signInWithCredential(phoneAuthCredential);
       print(
-          'Phone number automatically verified and user signed in: ${auth.currentUser!.uid}');
+          'Phone number automatically verified and user signed in: ${auth
+              .currentUser!.uid}');
     };
 
     PhoneVerificationFailed verificationFailed =
         (FirebaseAuthException authException) {
       print(
-          'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}');
+          'Phone number verification failed. Code: ${authException
+              .code}. Message: ${authException.message}');
     };
 
     PhoneCodeSent codeSent =
@@ -167,109 +169,4 @@ class _RegisterWithMobileScreen2State extends State<RegisterWithMobileScreen2>
       print('Failed to sign in: ' + e.toString());
     }
   }
-
-//
-// Future<void> sendOTP() async {
-//   setState(() {
-//     // loading = true;
-//   });
-//   await auth.verifyPhoneNumber(
-//     phoneNumber: mobileEditingController.text.toString(),
-//     verificationCompleted: (PhoneAuthCredential credential) async {
-//       await auth.signInWithCredential(credential).then((value) {
-//         print("You are logged in successfully");
-//         Navigator.pushAndRemoveUntil(
-//           context,
-//           MaterialPageRoute(
-//               builder: (BuildContext context) => RegisterScreen()),
-//           ModalRoute.withName('/'),
-//         );
-//       });
-//     },
-//     verificationFailed: (FirebaseAuthException e) {
-//       print('verificationFailed error message => ${e.message}');
-//     },
-//     codeSent: (String verificationId, int? resendToken) async {
-//       setState(() {
-//         this.verificationId = verificationId;
-//       });
-//
-//       print("You are logged in, codeSent " + '${verificationId.toString()}');
-//
-//       Navigator.pushReplacement(
-//         context,
-//         MaterialPageRoute(
-//           builder: (context) => CodeActiveScreen(
-//             verificationId: this.verificationId,
-//             phone: mobileEditingController.text.toString(),
-//           ),
-//         ),
-//       );
-//     },
-//     codeAutoRetrievalTimeout: (String verificationId) {},
-//   );
-// }
-
-// Future<void> resetPassword() async {
-//   // bool state = await AutController()
-//   //     .verifyOTP(context: context, code: getVerificationCode());
-//   // if (state) {
-//   //   Navigator.pushNamed(context, "/code_active_screen");
-//   // }
-//   setState(() {
-//     loding = true;
-//   });
-//   PhoneAuthCredential credential = PhoneAuthProvider.credential(
-//       verificationId: widget.virficationid, smsCode: getVerificationCode());
-//
-//   await auth.signInWithCredential(credential).then(
-//     (value) async {
-//       setState(() {
-//         loding = false;
-//       });
-//       print("You are logged in successfully then");
-//       var user = auth.currentUser!.uid;
-//       print("You are logged in successfully then " + user.toString());
-//       showSnackBar(
-//         context,
-//         message: 'You are logged in successfully',
-//         error: false,
-//       );
-//       print(credential.token.toString() + "idtoken phone then");
-//       print(credential.signInMethod.toString() + "idtoken phone then");
-//       bool state = await AutApiController()
-//           .register(context, password: user, username: widget.phone);
-//       if (state) {
-//         await SharedPrefController().setlogen();
-//         Navigator.pushAndRemoveUntil(
-//           context,
-//           MaterialPageRoute(builder: (BuildContext context) => MainScreens()),
-//           ModalRoute.withName('/'),
-//         );
-//       }
-//
-//       // Navigator.pushNamed(context, "/home_screen");
-//     },
-//   ).whenComplete(
-//     () async {
-//       setState(() {
-//         loding = false;
-//       });
-//       var user = auth.currentUser!.uid;
-//       print(credential.providerId.toString() + "idtoken phone whenComplete");
-//       bool state = await AutApiController()
-//           .register(context, password: user, username: widget.phone);
-//       if (state) {
-//         Navigator.pushAndRemoveUntil(
-//           context,
-//           MaterialPageRoute(
-//               builder: (BuildContext context) => RegisterScreen()),
-//           ModalRoute.withName('/'),
-//         );
-//       }
-//
-//       // Navigator.pushNamed(context, "/home_screen");
-//     },
-//   );
-// }
 }
